994ff70d177cbac5b46619f84b9cd9c51dcb297f
egesabanci <54193428+Egesabanci@users.noreply.github.com>
2020-07-13 04:58:59 +0300

    Update statistic.py

--- linear/statistic.py
+++ linear/statistic.py
@@ -3,7 +3,8 @@ import statistics as st
 
 def correlation(x_set, y_set):
 	eq1 = np.dot(x_set, y_set) - sum(x_set) * sum(y_set) / len(x_set)
-	eq2 = np.sqrt((sum([i ** 2 for i in x_set]) - sum(x_set) ** 2 / len(x_set)) * (sum([i ** 2 for i in y_set]) - sum(y_set) ** 2 / len(y_set)))
+	eq2 = np.sqrt((sum([i ** 2 for i in x_set]) - sum(x_set) ** 2 / len(x_set)) \ 
+	      * (sum([i ** 2 for i in y_set]) - sum(y_set) ** 2 / len(y_set)))
 	return eq1 / eq2
 
 def standard_deviation(arr : list) -> float:
